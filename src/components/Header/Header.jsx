76b07c24d03ebc1031e335de6bd322ffa4085dfa
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-09-29 13:02:19 +0200

    initial commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,46 +1,68 @@
-import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import logo from '../../assets/star-wars-logo.jpg'
-import LoginNav from '../../pages/Login/LoginNav'
-import './header.css'
-import { Link } from 'react-router-dom'
-import MyNavbar from './MyNavbar'
-import {LinkContainer} from 'react-router-bootstrap'
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../../assets/star-wars-logo.jpg";
+// import LoginNav from "../../pages/Login/LoginNav";
+import "./header.css";
+import { Link } from "react-router-dom";
+import MyNavbar from "./MyNavbar";
+// import { LinkContainer } from "react-router-bootstrap";
 
 const Header = ({ setShowCard, loggedIn, setLoggedIn }) => {
+  const handleClick = () => {
+    setLoggedIn(false);
+  };
+  const conditionalNav = (
+    <Nav>
+      {loggedIn ? (
+        <Link
+          onClick={handleClick}
+          to="/home"
+          className="nav-link login-nav-link"
+        >
+          LOG OUT
+        </Link>
+      ) : (
+        <>
+          <Link className="login-nav-link navbar-link p-1" to="/loginform">
+            LOG IN
+          </Link>
+          <div className="login-nav-link p-1">//</div>
+          <Link className="login-nav-link navbar-link p-1" to="/signupform">
+            SIGN UP
+          </Link>
+        </>
+      )}
+    </Nav>
+  );
+
   return (
     <header className="header">
-      {/* <Nav className="d-flex p-3 justify-content-center">
-        <Navbar.Brand className="m-0">
-          <Link className="navbar-link p-2" to="/home">
-            <img width="220px" src={logo} alt="logo" />
-          </Link>
-        </Navbar.Brand>
-        <LoginNav loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-      </Nav> */}
-      <Navbar expand="md">
-        <Container bg="transparent">
-          <Navbar.Brand className="m-0">
+      <Navbar className="navbar-box" expand="md">
+        <Container
+          fluid
+          className="navbar-container-box position-relative justify-content-end"
+          bg="transparent"
+        >
+          <Navbar.Brand className="navbar-brand-box m-0">
             <Link className="navbar-link p-2" to="/home">
               <img width="220px" src={logo} alt="logo" />
             </Link>
           </Navbar.Brand>
-          <Navbar.Toggle className="bg-light" aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse className='justify-content-end mb-2' id="basic-navbar-nav">
-            <Nav className="text-center me-auto">
-              {/* <Nav.Link className="login-nav-link">LOG IN</Nav.Link> */}
-              <Link className="login-nav-link navbar-link p-1" to="/loginform">LOG IN</Link>
-              <div className="login-nav-link p-1">//</div>
-              <Link className="login-nav-link navbar-link p-1" to="/signupform">SIGN UP</Link>
-              {/* <Nav.Link className="login-nav-link">SIGN UP</Nav.Link> */}
-            </Nav>
-          </Navbar.Collapse>
+          {/* <Navbar.Toggle
+            className="bg-light ms-auto"
+            aria-controls="basic-navbar-nav"
+          />
+          <Navbar.Collapse
+            className="flex-grow-0 text-center"
+            id="basic-navbar-nav"
+          >
+          </Navbar.Collapse> */}
+          {conditionalNav}
         </Container>
       </Navbar>
-      {/* <LoginNav loggedIn={loggedIn} setLoggedIn={setLoggedIn} /> */}
       <MyNavbar setShowCard={setShowCard} />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
