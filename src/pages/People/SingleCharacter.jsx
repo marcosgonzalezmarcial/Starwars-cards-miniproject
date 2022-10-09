4a531a63cb6a12d38f78282bdcab21e4e90c0aee
marcosgonzalezmarcial <marcospgonzalezm@gmail.com>
2022-10-09 20:04:57 +0200

    adding initial values to SingleCharacter component

--- src/pages/People/SingleCharacter.jsx
+++ src/pages/People/SingleCharacter.jsx
@@ -8,19 +8,45 @@ import PeopleShips from './PeopleShips'
 // import Pilots from './Pilots/Pilots'
 
 const SingleShip = () => {
-	const [person, setPerson] = useState({})
+	const [person, setPerson] = useState({
+		birth_year: '19 BBY',
+		eye_color: 'Blue',
+		films: ['https://swapi.dev/api/films/1/'],
+		gender: 'Male',
+		hair_color: 'Blond',
+		height: '172',
+		homeworld: 'https://swapi.dev/api/planets/1/',
+		mass: '77',
+		name: 'Luke Skywalker',
+		skin_color: 'Fair',
+		created: '2014-12-09T13:50:51.644000Z',
+		edited: '2014-12-10T13:52:43.172000Z',
+		species: ['https://swapi.dev/api/species/1/'],
+		starships: ['https://swapi.dev/api/starships/12/'],
+		url: 'https://swapi.dev/api/people/1/',
+		vehicles: ['https://swapi.dev/api/vehicles/14/'],
+	})
 	const [isLoading, setIsLoading] = useState(true)
 	const [img, setImg] = useState('')
 
 	let { id } = useParams()
-
 	useEffect(() => {
 		setIsLoading(true)
 		fetch(`https://swapi.dev/api/people/${id}/`)
 			.then(res => res.json())
 			.then(data => {
 				setPerson(data)
+				// setIsLoading(false)
 			})
+	}, [id])
+
+	useEffect(() => {
+		setIsLoading(true)
+		// fetch(`https://swapi.dev/api/people/${id}/`)
+		// 	.then(res => res.json())
+		// 	.then(data => {
+		// 		setPerson(data)
+		// 	})
 		fetch(`https://miadil.github.io/starwars-api/api/id/${id}.json`)
 			.then(res => res.json())
 			.then(data => {
@@ -28,16 +54,6 @@ const SingleShip = () => {
 				setIsLoading(false)
 			})
 	}, [id])
-
-	// useEffect(() => {
-	// 	setIsLoading(true)
-	// 	fetch(`https://swapi.dev/api/people/${id}/`)
-	// 		.then(res => res.json())
-	// 		.then(data => {
-	// 			setPerson(data)
-	// 			setIsLoading(false)
-	// 		})
-	// }, [id])
 
 	return (
 		<>
