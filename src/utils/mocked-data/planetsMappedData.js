9526322c645e83f451c8d7ce6f397df758c63428
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-11-03 18:34:32 +0100

    plannetsMappedData updated

---
+++ src/utils/mocked-data/planetsMappedData.js
@@ -0,0 +1,356 @@
+export default [
+  {
+    title: 'Tatooine',
+    id: 1,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667493462/Starwars%20miniproject/Planets/Tatooine_1_hf6gpz.webp'
+  },
+  {
+    title: 'Alderaan',
+    id: 2,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667493564/Starwars%20miniproject/Planets/Alderaan_ivwjwb.webp'
+  },
+  {
+    title: 'Yavin IV',
+    id: 3,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667493762/Starwars%20miniproject/Planets/Yavin4_ryt5jt.webp'
+  },
+  {
+    title: 'Hoth',
+    id: 4,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667493934/Starwars%20miniproject/Planets/Hoth_hbfjt1.webp'
+  },
+  {
+    title: 'Dagobah',
+    id: 5,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667494061/Starwars%20miniproject/Planets/Dagobah_fszuhn.webp'
+  },
+  {
+    title: 'Bespin',
+    id: 6,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667494189/Starwars%20miniproject/Planets/Bespin_qrrdno.webp'
+  },
+  {
+    title: 'Endor',
+    id: 7,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667494311/Starwars%20miniproject/Planets/Endor_nwgasl.webp'
+  },
+  {
+    title: 'Naboo',
+    id: 8,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667494401/Starwars%20miniproject/Planets/Naboo_xtkund.webp'
+  },
+  {
+    title: 'Coruscant',
+    id: 9,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667494547/Starwars%20miniproject/Planets/Coruscant_cbm6t5.webp'
+  },
+  {
+    title: 'Kamino',
+    id: 10,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667494662/Starwars%20miniproject/Planets/Kamino_fekk8r.webp'
+  },
+  {
+    title: 'Geonosis',
+    id: 11,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667494872/Starwars%20miniproject/Planets/geonosis_qoew5c.webp'
+  },
+  {
+    title: 'Utapau',
+    id: 12,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667495070/Starwars%20miniproject/Planets/Utapau_dagevn.webp'
+  },
+  {
+    title: 'Mustafar',
+    id: 13,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667495201/Starwars%20miniproject/Planets/Mustafar_onc2sz.webp'
+  },
+  {
+    title: 'Kashyyyk',
+    id: 14,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667495568/Starwars%20miniproject/Planets/Kashyyyk_erjuow.webp'
+  },
+  {
+    title: 'Polis Massa',
+    id: 15,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667495939/Starwars%20miniproject/Planets/PolisMassa_sf38eq.webp'
+  },
+  {
+    title: 'Mygeeto',
+    id: 16,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667496037/Starwars%20miniproject/Planets/Mygeeto_thirj2.webp'
+  },
+  {
+    title: 'Felucia',
+    id: 17,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667496248/Starwars%20miniproject/Planets/Felucia_demt5n.webp'
+  },
+  {
+    title: 'Cato Neimoidia',
+    id: 18,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667496349/Starwars%20miniproject/Planets/Cato_Neimoidia_zj2xf8.webp'
+  },
+  {
+    title: 'Saleucami',
+    id: 19,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667496497/Starwars%20miniproject/Planets/Saleucami_kr0q4x.webp'
+  },
+  {
+    title: 'Stewjon',
+    id: 20,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1667496811/Starwars%20miniproject/Planets/Stewjon_jxqc2l.webp'
+  },
+  {
+    title: 'Eriadu',
+    id: 21,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode6_oixxmo.webp'
+  },
+  {
+    title: 'Corellia',
+    id: 22,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode1_zqpgye.webp'
+  },
+  {
+    title: 'Rodia',
+    id: 23,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode2_wisdjr.webp'
+  },
+  {
+    title: 'Nal Hutta',
+    id: 24,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode3_j5erlx.webp'
+  },
+  {
+    title: 'Dantooine',
+    id: 25,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode4_qpbmuz.webp'
+  },
+  {
+    title: 'Bestine IV',
+    id: 26,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864466/Starwars%20miniproject/Films/episode5_nr1h8w.webp'
+  },
+  {
+    title: 'Ord Mantell',
+    id: 27,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode6_oixxmo.webp'
+  },
+  {
+    title: 'Trandosha',
+    id: 29,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode1_zqpgye.webp'
+  },
+  {
+    title: 'Socorro',
+    id: 30,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode2_wisdjr.webp'
+  },
+  {
+    title: 'Mon Cala',
+    id: 31,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode3_j5erlx.webp'
+  },
+  {
+    title: 'Chandrila',
+    id: 32,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode4_qpbmuz.webp'
+  },
+  {
+    title: 'Sullust',
+    id: 33,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864466/Starwars%20miniproject/Films/episode5_nr1h8w.webp'
+  },
+  {
+    title: 'Toydaria',
+    id: 34,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode6_oixxmo.webp'
+  },
+  {
+    title: 'Malastare',
+    id: 35,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode1_zqpgye.webp'
+  },
+  {
+    title: 'Dathomir',
+    id: 36,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode2_wisdjr.webp'
+  },
+  {
+    title: 'Ryloth',
+    id: 37,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode3_j5erlx.webp'
+  },
+  {
+    title: 'Aleen Minor',
+    id: 38,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode4_qpbmuz.webp'
+  },
+  {
+    title: 'Vulpter',
+    id: 39,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864466/Starwars%20miniproject/Films/episode5_nr1h8w.webp'
+  },
+  {
+    title: 'Troiken',
+    id: 40,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode6_oixxmo.webp'
+  },
+  {
+    title: 'Tund',
+    id: 41,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode1_zqpgye.webp'
+  },
+  {
+    title: 'Haruun Kal',
+    id: 42,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode2_wisdjr.webp'
+  },
+  {
+    title: 'Cerea',
+    id: 43,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode3_j5erlx.webp'
+  },
+  {
+    title: 'Glee Anselm',
+    id: 44,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode4_qpbmuz.webp'
+  },
+  {
+    title: 'Iridonia',
+    id: 45,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864466/Starwars%20miniproject/Films/episode5_nr1h8w.webp'
+  },
+  {
+    title: 'Tholoth',
+    id: 46,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode6_oixxmo.webp'
+  },
+  {
+    title: 'Iktotch',
+    id: 47,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode1_zqpgye.webp'
+  },
+  {
+    title: 'Quermia',
+    id: 48,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode2_wisdjr.webp'
+  },
+  {
+    title: 'Dorin',
+    id: 49,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode3_j5erlx.webp'
+  },
+  {
+    title: 'Champala',
+    id: 50,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode4_qpbmuz.webp'
+  },
+  {
+    title: 'Mirial',
+    id: 51,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864466/Starwars%20miniproject/Films/episode5_nr1h8w.webp'
+  },
+  {
+    title: 'Serenno',
+    id: 52,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode6_oixxmo.webp'
+  },
+  {
+    title: 'Concord Dawn',
+    id: 53,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode1_zqpgye.webp'
+  },
+  {
+    title: 'Zolan',
+    id: 54,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode2_wisdjr.webp'
+  },
+  {
+    title: 'Ojom',
+    id: 55,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode3_j5erlx.webp'
+  },
+  {
+    title: 'Skako',
+    id: 56,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode4_qpbmuz.webp'
+  },
+  {
+    title: 'Muunilinst',
+    id: 57,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864466/Starwars%20miniproject/Films/episode5_nr1h8w.webp'
+  },
+  {
+    title: 'Shili',
+    id: 58,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864463/Starwars%20miniproject/Films/episode6_oixxmo.webp'
+  },
+  {
+    title: 'Kalee',
+    id: 59,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode1_zqpgye.webp'
+  },
+  {
+    title: 'Umbara',
+    id: 60,
+    imgUrl:
+      'https://res.cloudinary.com/dn9mr6kwc/image/upload/q_auto:low/v1666864464/Starwars%20miniproject/Films/episode2_wisdjr.webp'
+  }
+]
