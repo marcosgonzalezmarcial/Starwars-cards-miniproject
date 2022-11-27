967295bdec597898156bd30cf55c6e2fa54a03b3
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-11-27 08:58:52 +0000

    Add scroll to top when navigation trough pages occurs

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './base.scss'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 
 const container = document.getElementById('app')
 const root = createRoot(container)
 root.render(
+  <>
   <BrowserRouter>
+    <ScrollToTop />
     <App tab="home" />
   </BrowserRouter>
+  </>
 )
