a645acdb0bbb1dc0c43290224091d2e0def2d7f7
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-12-05 16:35:24 +0100

    Change header logo
    * removed !important
    
    * removing important and fixing minor styling bugs on header
    
    * removed important & fix light-saber div width
    
    * change logo on different screen sizes
    
    * change header logo on different screen sizes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,66 +1,71 @@
-import React, { useState } from 'react'
-import { Navbar, Container, Button } from 'react-bootstrap'
-import logo from '../../assets/sw_logo_mobile.png'
-import { Link, useNavigate } from 'react-router-dom'
-import SectionNav from './SectionNav'
-import LoginNav from './LoginNav'
-import SearchIcon from '../SearchIcon/SearchIcon'
-import SearchModal from '../SearchModal/SearchModal'
-import './Header.scss'
+import React, { useState } from "react";
+import { Navbar } from "react-bootstrap";
+import smallLogo from "../../assets/sw_logo_mobile.png";
+import bigLogo from "../../assets/star-wars-logo.jpg";
+import { Link, useNavigate } from "react-router-dom";
+import SectionNav from "./SectionNav";
+import LoginNav from "./LoginNav";
+import SearchIcon from "../SearchIcon/SearchIcon";
+import SearchModal from "../SearchModal/SearchModal";
+import "./Header.scss";
 
 const Header = ({ loggedIn, setLoggedIn }) => {
-	const [modalShow, setModalShow] = useState(false)
-	let navigate = useNavigate()
+  const [modalShow, setModalShow] = useState(false);
+  let navigate = useNavigate();
 
-	const handleSearchClick = () => {
-		if (loggedIn) {
-			setModalShow(true)
-		} else {
-			navigate('/login')
-		}
-	}
-	return (
-		<header className="header">
-			<Navbar variant="dark" className="navbar-box py-1 py-md-3" expand="md">
-				<Container
-					fluid
-					className="navbar-container-box text-center position-relative justify-content-end px-0"
-				>
-					<Navbar.Toggle
-						className="navbar-toggle-btn me-auto"
-						aria-controls="basic-navbar-nav"
-					/>
-					<SearchModal show={modalShow} onHide={() => setModalShow(false)} />
+  const handleSearchClick = () => {
+    if (loggedIn) {
+      setModalShow(true);
+    } else {
+      navigate("/login");
+    }
+  };
+  return (
+    <header className="header">
+      <Navbar variant="dark" className="py-1" expand="md">
+        <div className="navbar-container text-center position-relative justify-content-end px-0">
+          <Navbar.Toggle
+            className="navbar-toggle-btn me-auto"
+            aria-controls="basic-navbar-nav"
+          />
+          <SearchModal show={modalShow} onHide={() => setModalShow(false)} />
 
-					<Navbar.Brand className="m-0 p-3">
-						<Link className="navbar-link p-2" to="/home">
-							<img width="220px" className="logo-img" src={logo} alt="logo" />
-						</Link>
-					</Navbar.Brand>
+          <Navbar.Brand className="m-0 p-3 p-md-0">
+            <Link className="navbar-link p-2" to="/home">
+              <picture>
+                <source media="(max-width: 768px)" srcset={smallLogo} />
+                <source media="(min-width: 769px)" srcset={bigLogo} />
+                <img
+                  // width="220px"
+                  className="logo-img"
+                  src={smallLogo}
+                  alt="logo"
+                />
+              </picture>
+              {/* <img width="220px" className="logo-img" src={logo} alt="logo" /> */}
+            </Link>
+          </Navbar.Brand>
 
-					<div className="ms-auto me-md-auto ms-md-0 search-icon-container">
-						<Button
-							// as="div"
-							className="search-icon-btn"
-							onClick={() => handleSearchClick()}
-						>
-							<SearchIcon onClick={prev => setModalShow(!prev)} />
-						</Button>
-					</div>
+          <button
+            className="search-icon-btn ms-auto me-md-auto ms-md-0"
+            onClick={() => handleSearchClick()}
+          >
+            <SearchIcon onClick={(prev) => setModalShow(!prev)} />
+          </button>
 
-					<span></span>
-					<Navbar.Collapse
-						className="navbar-collapse-box"
-						id="responsive-navbar-nav"
-					>
-						<LoginNav loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-					</Navbar.Collapse>
-					<span></span>
-				</Container>
-			</Navbar>
-			<SectionNav />
-		</header>
-	)
-}
+          <span></span>
+          <Navbar.Collapse
+            className="navbar-collapse-box"
+            id="responsive-navbar-nav"
+          >
+            <LoginNav loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+          </Navbar.Collapse>
+          <span></span>
+        </div>
+      </Navbar>
+      <SectionNav />
+    </header>
+  );
+};
 
-export default Header
+export default Header;
