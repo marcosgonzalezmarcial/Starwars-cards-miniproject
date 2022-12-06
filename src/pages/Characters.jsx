e3e1ddcacec1dd598be5039c92debcc63f9e6451
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-12-06 07:34:43 +0000

    remove automatic alphabetic order

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,85 +1,87 @@
-import { useState, useEffect } from 'react'
-import InfiniteScroll from 'react-infinite-scroll-component'
-import { useNavigate } from 'react-router-dom'
-import { useSearch } from '../hooks/useSearch'
-import { Spinner } from '../components/Spinner/Spinner'
-import '../styles.scss'
-import { TYPE_OF_DATA } from '../constants'
-import { getTransformedDataArray } from '../services/getTransformedDataArray'
-import { sortObjItems } from '../utils/sortItems'
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useNavigate } from "react-router-dom";
+import { useSearch } from "../hooks/useSearch";
+import { Spinner } from "../components/Spinner/Spinner";
+import "../styles.scss";
+import { TYPE_OF_DATA } from "../constants";
+import { getTransformedDataArray } from "../services/getTransformedDataArray";
+// import { sortObjItems } from "../utils/sortItems";
 
 const Characters = () => {
-	const [page, setPage] = useState(1)
-	const [characters, setCharacters] = useState([])
-	const { searchItems } = useSearch()
+  const [page, setPage] = useState(1);
+  const [characters, setCharacters] = useState([]);
+  const { searchItems } = useSearch();
 
-	let navigate = useNavigate()
+  let navigate = useNavigate();
 
-	useEffect(() => {
-		getTransformedDataArray({ page, typeOfData: TYPE_OF_DATA.PEOPLE })
-			.then(data => {
-				//checking data is not null
-				data && setCharacters(prev => sortObjItems([...prev, ...data]))
-			})
-			.catch(error => {
-				console.log(error)
-			})
-	}, [page])
+  useEffect(() => {
+    getTransformedDataArray({ page, typeOfData: TYPE_OF_DATA.PEOPLE })
+      .then((data) => {
+        //checking data is not null
+        data && setCharacters((prev) => [...prev, ...data]);
+        // sorting items may be applied in future iterations
+        // data && setCharacters((prev) => sortObjItems([...prev, ...data]));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [page]);
 
-	const handleClick = e => {
-		const personSelected = e.target.textContent
-		navigate(`${personSelected.replaceAll(' ', '~')}`)
-	}
+  const handleClick = (e) => {
+    const personSelected = e.target.textContent;
+    navigate(`${personSelected.replaceAll(" ", "~")}`);
+  };
 
-	return (
-		<>
-			{searchItems.length > 0 ? (
-				<div className="my-3 my-md-4 grid-container">
-					{searchItems.map(character => (
-						<div key={character.id} className="grid-element-card">
-							<div className="grid-card-hero">
-								<img
-									className="grid-card-hero-img"
-									src={character.image}
-									alt={character.name}
-								/>
-							</div>
-							<div className="text-secondary p-3 grid-card-info bg-dark">
-								<h4 onClick={handleClick}>{character.name}</h4>
-								<p>{character.species}</p>
-							</div>
-						</div>
-					))}
-				</div>
-			) : (
-				<InfiniteScroll
-					dataLength={characters.length}
-					next={() => setPage(prev => characters.length < 87 && prev + 1)}
-					hasMore={characters.length < 87 && true}
-					loader={<Spinner />}
-					className={`my-3 my-md-4 ${
-						characters.length > 0 ? 'grid-container' : ''
-					}`}
-				>
-					{characters.map(character => (
-						<div key={character.id} className="grid-element-card">
-							<div className="grid-card-hero">
-								<img
-									className="grid-card-hero-img"
-									src={character.image}
-									alt={character.name}
-								/>
-							</div>
-							<div className="text-secondary p-3 grid-card-info bg-dark">
-								<h4 onClick={handleClick}>{character.name}</h4>
-								<p>{character.species}</p>
-							</div>
-						</div>
-					))}
-				</InfiniteScroll>
-			)}
-		</>
-	)
-}
+  return (
+    <>
+      {searchItems.length > 0 ? (
+        <div className="my-3 my-md-4 grid-container">
+          {searchItems.map((character) => (
+            <div key={character.id} className="grid-element-card">
+              <div className="grid-card-hero">
+                <img
+                  className="grid-card-hero-img"
+                  src={character.image}
+                  alt={character.name}
+                />
+              </div>
+              <div className="text-secondary p-3 grid-card-info bg-dark">
+                <h4 onClick={handleClick}>{character.name}</h4>
+                <p>{character.species}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <InfiniteScroll
+          dataLength={characters.length}
+          next={() => setPage((prev) => characters.length < 87 && prev + 1)}
+          hasMore={characters.length < 87 && true}
+          loader={<Spinner />}
+          className={`my-3 my-md-4 ${
+            characters.length > 0 ? "grid-container" : ""
+          }`}
+        >
+          {characters.map((character) => (
+            <div key={character.id} className="grid-element-card">
+              <div className="grid-card-hero">
+                <img
+                  className="grid-card-hero-img"
+                  src={character.image}
+                  alt={character.name}
+                />
+              </div>
+              <div className="text-secondary p-3 grid-card-info bg-dark">
+                <h4 onClick={handleClick}>{character.name}</h4>
+                <p>{character.species}</p>
+              </div>
+            </div>
+          ))}
+        </InfiniteScroll>
+      )}
+    </>
+  );
+};
 
-export default Characters
+export default Characters;
--- src/pages/Planets.jsx
+++ src/pages/Planets.jsx
@@ -1,86 +1,88 @@
-import { useState, useEffect } from 'react'
-import InfiniteScroll from 'react-infinite-scroll-component'
-import { useNavigate } from 'react-router-dom'
-import { useSearch } from '../hooks/useSearch.js'
-import { Spinner } from '../components/Spinner/Spinner'
-import '../styles.scss'
-import { getTransformedDataArray } from '../services/getTransformedDataArray'
-import { TYPE_OF_DATA } from '../constants'
-import { sortObjItems } from '../utils/sortItems.js'
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useNavigate } from "react-router-dom";
+import { useSearch } from "../hooks/useSearch.js";
+import { Spinner } from "../components/Spinner/Spinner";
+import "../styles.scss";
+import { getTransformedDataArray } from "../services/getTransformedDataArray";
+import { TYPE_OF_DATA } from "../constants";
+// import { sortObjItems } from "../utils/sortItems.js";
 
 const Planets = () => {
-	const [page, setPage] = useState(1)
-	const [planets, setPlanets] = useState([])
-	const { searchItems } = useSearch()
+  const [page, setPage] = useState(1);
+  const [planets, setPlanets] = useState([]);
+  const { searchItems } = useSearch();
 
-	let navigate = useNavigate()
+  let navigate = useNavigate();
 
-	useEffect(() => {
-		getTransformedDataArray({ page, typeOfData: TYPE_OF_DATA.PLANETS })
-			.then(data => {
-				//checking data is not null
-				data && setPlanets(prev => sortObjItems([...prev, ...data]))
-				
-			})
-			.catch(error => {
-				console.log(error)
-			})
-	}, [page])
+  useEffect(() => {
+    getTransformedDataArray({ page, typeOfData: TYPE_OF_DATA.PLANETS })
+      .then((data) => {
+        //checking data is not null
 
-	const handleClick = e => {
-		const planetSelected = e.target.textContent
-		navigate(`${planetSelected.replaceAll(' ', '~')}`)
-	}
+        data && setPlanets((prev) => [...prev, ...data]);
+        // sorting items may be applied in future iterations
+        // data && setPlanets((prev) => sortObjItems([...prev, ...data]));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [page]);
 
-	return (
-		<>
-			{searchItems.length > 0 ? (
-				<div className="my-3 my-md-4 grid-container">
-					{searchItems.map(planet => (
-						<div key={planet.name} className="grid-element-card">
-							<div className="grid-card-hero">
-								<img
-									className="grid-card-hero-img"
-									src={planet.imgUrl}
-									alt={planet.name}
-								/>
-							</div>
-							<div className="text-secondary bg-dark p-3 grid-card-info">
-								<h4 onClick={handleClick}>{planet.name}</h4>
-								<p>{planet.climate}</p>
-							</div>
-						</div>
-					))}
-				</div>
-			) : (
-				<InfiniteScroll
-					dataLength={planets.length}
-					next={() => setPage(prev => planets.length < 59 && prev + 1)}
-					hasMore={planets.length < 59 && true}
-					loader={<Spinner />}
-					className={`my-3 my-md-4 ${
-						planets.length > 0 ? 'grid-container' : ''
-					}`}
-				>
-					{planets.map(planet => (
-						<div key={planet.name} className="grid-element-card">
-							<div className="grid-card-hero">
-								<img
-									className="grid-card-hero-img"
-									src={planet.imgUrl}
-									alt={planet.name}
-								/>
-							</div>
-							<div className="text-secondary bg-dark p-3 grid-card-info">
-								<h4 onClick={handleClick}>{planet.name}</h4>
-								<p>{planet.climate}</p>
-							</div>
-						</div>
-					))}
-				</InfiniteScroll>
-			)}
-		</>
-	)
-}
+  const handleClick = (e) => {
+    const planetSelected = e.target.textContent;
+    navigate(`${planetSelected.replaceAll(" ", "~")}`);
+  };
 
-export default Planets
+  return (
+    <>
+      {searchItems.length > 0 ? (
+        <div className="my-3 my-md-4 grid-container">
+          {searchItems.map((planet) => (
+            <div key={planet.name} className="grid-element-card">
+              <div className="grid-card-hero">
+                <img
+                  className="grid-card-hero-img"
+                  src={planet.imgUrl}
+                  alt={planet.name}
+                />
+              </div>
+              <div className="text-secondary bg-dark p-3 grid-card-info">
+                <h4 onClick={handleClick}>{planet.name}</h4>
+                <p>{planet.climate}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <InfiniteScroll
+          dataLength={planets.length}
+          next={() => setPage((prev) => planets.length < 59 && prev + 1)}
+          hasMore={planets.length < 59 && true}
+          loader={<Spinner />}
+          className={`my-3 my-md-4 ${
+            planets.length > 0 ? "grid-container" : ""
+          }`}
+        >
+          {planets.map((planet) => (
+            <div key={planet.name} className="grid-element-card">
+              <div className="grid-card-hero">
+                <img
+                  className="grid-card-hero-img"
+                  src={planet.imgUrl}
+                  alt={planet.name}
+                />
+              </div>
+              <div className="text-secondary bg-dark p-3 grid-card-info">
+                <h4 onClick={handleClick}>{planet.name}</h4>
+                <p>{planet.climate}</p>
+              </div>
+            </div>
+          ))}
+        </InfiniteScroll>
+      )}
+    </>
+  );
+};
+
+export default Planets;
