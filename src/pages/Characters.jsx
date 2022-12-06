75af3b650ad5f36081996e69a85523672b6a24b8
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-12-06 12:43:50 +0000

    add navigation clicking the card element on the grid

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSearch } from "../hooks/useSearch";
 import { Spinner } from "../components/Spinner/Spinner";
 import "../styles.scss";
@@ -12,8 +12,6 @@ const Characters = () => {
   const [page, setPage] = useState(1);
   const [characters, setCharacters] = useState([]);
   const { searchItems } = useSearch();
-
-  let navigate = useNavigate();
 
   useEffect(() => {
     getTransformedDataArray({ page, typeOfData: TYPE_OF_DATA.PEOPLE })
@@ -27,11 +25,6 @@ const Characters = () => {
         console.log(error);
       });
   }, [page]);
-
-  const handleClick = (e) => {
-    const personSelected = e.target.textContent;
-    navigate(`${personSelected.replaceAll(" ", "~")}`);
-  };
 
   return (
     <>
@@ -47,7 +40,7 @@ const Characters = () => {
                 />
               </div>
               <div className="text-secondary p-3 grid-card-info bg-dark">
-                <h4 onClick={handleClick}>{character.name}</h4>
+                <h4>{character.name}</h4>
                 <p>{character.species}</p>
               </div>
             </div>
@@ -64,19 +57,21 @@ const Characters = () => {
           }`}
         >
           {characters.map((character) => (
-            <div key={character.id} className="grid-element-card">
-              <div className="grid-card-hero">
-                <img
-                  className="grid-card-hero-img"
-                  src={character.image}
-                  alt={character.name}
-                />
+            <Link key={character.name} to={character.name}>
+              <div className="grid-element-card">
+                <div className="grid-card-hero">
+                  <img
+                    className="grid-card-hero-img"
+                    src={character.image}
+                    alt={character.name}
+                  />
+                </div>
+                <div className="text-secondary p-3 grid-card-info bg-dark">
+                  <h4>{character.name}</h4>
+                  <p>{character.species}</p>
+                </div>
               </div>
-              <div className="text-secondary p-3 grid-card-info bg-dark">
-                <h4 onClick={handleClick}>{character.name}</h4>
-                <p>{character.species}</p>
-              </div>
-            </div>
+            </Link>
           ))}
         </InfiniteScroll>
       )}
