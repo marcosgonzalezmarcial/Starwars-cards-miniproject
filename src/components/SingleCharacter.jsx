3f1c00b1ccb05a31dc0afa5c41e2ba2a11bff729
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-12-13 07:57:38 +0100

    Fix view more feature (#20) (#21)
    * fix-view-more-feature work in progress
    
    * fix-view-more-feature work in progress
    
    * fix-view-more-feature work in progress
    
    * view-more-feature finished
    
    * Update SingleCharacter.jsx and view-more.scss

--- src/components/SingleCharacter.jsx
+++ src/components/SingleCharacter.jsx
@@ -1,56 +1,74 @@
-import { useEffect, useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
-import { useNavigate, useParams } from 'react-router-dom'
-import ListOfFilms from './ListOfFilms'
-import ListOfShips from './ListOfShips'
-import { urlStringify } from '../utils/urlStringify'
-import { Spinner } from './Spinner/Spinner'
-import { transformDataArray } from '../utils/transformDataArray'
-import { peopleMockedData } from '../utils/mocked-data'
-import { fetchItem } from '../services/fetchItem'
-import { TYPE_OF_DATA } from '../constants'
-import './single-item-page-styles.scss'
+import { useEffect, useRef, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import ListOfFilms from "./ListOfFilms";
+import ListOfShips from "./ListOfShips";
+import { urlStringify } from "../utils/urlStringify";
+import { Spinner } from "./Spinner/Spinner";
+import { transformDataArray } from "../utils/transformDataArray";
+import { peopleMockedData } from "../utils/mocked-data";
+import { fetchItem } from "../services/fetchItem";
+import { TYPE_OF_DATA } from "../constants";
+import "./single-item-page-styles.scss";
+import "./view-more.scss";
 
 const SingleCharacter = () => {
-  const [character, setCharacter] = useState({})
-  const [isLoading, setIsLoading] = useState(false)
-  let { characterName } = useParams()
-  let navigate = useNavigate()
+  const [character, setCharacter] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [dynamicSize, setDynamicSize] = useState({});
 
+  let { characterName } = useParams();
+  let navigate = useNavigate();
+
+  const mainRef = useRef(null);
+
+  //obvserving the size of the ListOfFilms container
   useEffect(() => {
-    setIsLoading(true)
-    const newPerson = urlStringify(characterName)
-    const { id } = peopleMockedData.find((person) => person.name === newPerson)
+    if (!mainRef || isLoading) return; // wait for the elementRef to be available and loading finishes
+    const resizeObserver = new ResizeObserver((entries) => {
+      setDynamicSize({
+        mainWidth: entries[0].contentRect.width,
+      });
+    });
+    resizeObserver.observe(mainRef.current);
+    return () => resizeObserver.disconnect(); // clean up
+  }, [isLoading]);
+
+  // fetch character
+  useEffect(() => {
+    setIsLoading(true);
+    const newPerson = urlStringify(characterName);
+    const { id } = peopleMockedData.find((person) => person.name === newPerson);
 
     fetchItem({ id, typeOfData: TYPE_OF_DATA.PEOPLE })
       .then((item) => {
         const [transformedCharacterData] = transformDataArray({
           // fetched data must be an array for implementation requirements
           fetchedData: [item],
-          typeOfData: TYPE_OF_DATA.PEOPLE
-        })
-        setCharacter(transformedCharacterData)
+          typeOfData: TYPE_OF_DATA.PEOPLE,
+        });
+        setCharacter(transformedCharacterData);
       })
       .catch(console.log)
-      .finally(() => setIsLoading(false))
-  }, [characterName])
+      .finally(() => setIsLoading(false));
+  }, [characterName]);
 
   const handleClick = (e) => {
-    const planetSelected = e.target.textContent
-    navigate(`/planets/${planetSelected}`)
-  }
+    const planetSelected = e.target.textContent;
+    navigate(`/planets/${planetSelected}`);
+  };
 
   return (
     <>
       {isLoading ? (
         <Spinner />
       ) : (
-        <main className="main text-secondary">
+        <main ref={mainRef} className="main text-secondary">
           <div className="page-img-container">
             <img src={character.image} alt={character.name} />
           </div>
           <div className="page-description-container bg-dark p-2">
-            <h1 className="mb-3 pt-1 px-2">{character.name}</h1>
+            <h1 className="mb-2 mb-sm-3 pt-1 px-2">{character.name}</h1>
             <div className="px-2">
               <Row className="py-1">
                 <Col>
@@ -62,8 +80,7 @@ const SingleCharacter = () => {
                   <span>{character.birth_year}</span>
                 </Col>
               </Row>
-              <Row className="py-1"></Row>
-              <Row className="py-1">
+              <Row className="pt-1">
                 <Col>
                   <h3>Species</h3>
                   <span>{character.species}</span>
@@ -76,21 +93,41 @@ const SingleCharacter = () => {
                   </p>
                 </Col>
               </Row>
-              <Row className="py-1">
-                <Col className="pt-1 ">
-                  <div className="pt-1 flex-column cutoff-text">
-                    <h3 className="m-0 py-1">Appearances</h3>
+              <Row>
+                <Col className="py-1">
+                  <div className="flex-column cutoff-text">
+                    <h3 className="my-2">Appearances</h3>
                     <ListOfFilms filmsUrls={character.films} />
                   </div>
-                  <input type="checkbox" className="expand-btn" />
+                  {dynamicSize.mainWidth < 518 &&
+                    character.films?.length > 3 && (
+                      <input type="checkbox" className="expand-btn" />
+                    )}
+                  {/* {dynamicSize.mainWidth > 517 &&
+                    character.films?.length > 6 && (
+                      <input type="checkbox" className="expand-btn" />
+                    )} */}
                 </Col>
                 <Col className="pt-1">
-                  <h3 className="m-0 py-1">Ships</h3>
-                  {character.starships?.length > 0 ? (
-                    <ListOfShips shipsUrls={character.starships} />
+                  {character.starships?.length === 0 ? (
+                    <>
+                      <h3 className="my-2">Starships</h3>
+                      <span>No starships registered for this character</span>
+                    </>
                   ) : (
-                    <span>There aren't ships for this character</span>
+                    <div className="flex-column cutoff-text">
+                      <h3 className="my-2">Starships</h3>
+                      <ListOfShips shipsUrls={character.starships} />
+                    </div>
                   )}
+                  {dynamicSize.mainWidth < 518 &&
+                    character.starships?.length > 3 && (
+                      <input type="checkbox" className="expand-btn" />
+                    )}
+                  {dynamicSize.mainWidth > 517 &&
+                    character.starships?.length > 6 && (
+                      <input type="checkbox" className="expand-btn" />
+                    )}
                 </Col>
               </Row>
             </div>
@@ -98,7 +135,7 @@ const SingleCharacter = () => {
         </main>
       )}
     </>
-  )
-}
+  );
+};
 
-export default SingleCharacter
+export default SingleCharacter;
--- src/components/SingleFilm.jsx
+++ src/components/SingleFilm.jsx
@@ -1,59 +1,75 @@
-import { useEffect, useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
-import { filmsMockedData } from '../utils/mocked-data'
-import ListOfShips from './ListOfShips'
-import { urlStringify } from '../utils/urlStringify'
-import { Spinner } from './Spinner/Spinner'
-import { transformDataArray } from '../utils/transformDataArray'
-import { fetchItem } from '../services/fetchItem'
-import { TYPE_OF_DATA } from '../constants'
-import './single-item-page-styles.scss'
+import { useEffect, useRef, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { filmsMockedData } from "../utils/mocked-data";
+import ListOfShips from "./ListOfShips";
+import { urlStringify } from "../utils/urlStringify";
+import { Spinner } from "./Spinner/Spinner";
+import { transformDataArray } from "../utils/transformDataArray";
+import { fetchItem } from "../services/fetchItem";
+import { TYPE_OF_DATA } from "../constants";
+import "./single-item-page-styles.scss";
+import "./view-more.scss";
 
 const SingleFilm = () => {
-  const [film, setFilm] = useState({})
-  const [isLoading, setIsLoading] = useState(false)
+  const [film, setFilm] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [dynamicSize, setDynamicSize] = useState({});
 
-  let { filmTitle } = useParams()
+  let { filmTitle } = useParams();
 
-  const expandBtnStyles = {
-    marginLeft: 'calc(var(--bs-gutter-x) * 0.5)'
-  }
+  // const expandBtnStyles = {
+  //   marginLeft: "calc(var(--bs-gutter-x) * 0.5)",
+  // };
+  console.log(film);
+
+  const mainRef = useRef(null);
 
   useEffect(() => {
-    setIsLoading(true)
+    if (!mainRef || isLoading) return; // wait for the elementRef to be available and loading finishes
+    const resizeObserver = new ResizeObserver((entries) => {
+      setDynamicSize({
+        mainWidth: entries[0].contentRect.width,
+      });
+    });
+    resizeObserver.observe(mainRef.current);
+    return () => resizeObserver.disconnect(); // clean up
+  }, [isLoading]);
 
-    const filmTitleFromUrl = urlStringify(filmTitle)
+  useEffect(() => {
+    setIsLoading(true);
+
+    const filmTitleFromUrl = urlStringify(filmTitle);
 
     const { id } = filmsMockedData.find(
       (film) => film.title === filmTitleFromUrl
-    )
+    );
 
     fetchItem({ id, typeOfData: TYPE_OF_DATA.FILMS })
       .then((item) => {
         const [transformedFilmData] = transformDataArray({
           // fetched data must be an array for implementation requirements
           fetchedData: [item],
-          typeOfData: TYPE_OF_DATA.FILMS
-        })
+          typeOfData: TYPE_OF_DATA.FILMS,
+        });
         // console.log(transformedFilmData);
-        setFilm(transformedFilmData)
+        setFilm(transformedFilmData);
       })
       .catch(console.log)
-      .finally(() => setIsLoading(false))
-  }, [filmTitle])
+      .finally(() => setIsLoading(false));
+  }, [filmTitle]);
 
   return (
     <>
       {isLoading ? (
         <Spinner />
       ) : (
-        <main className="main text-secondary">
+        <main ref={mainRef} className="main text-secondary">
           <div className="page-img-container">
             <img src={film.imgUrl} alt={film.title} />
           </div>
           <div className="page-description-container bg-dark p-2">
-            <h1 className="mb-3 pt-1 px-2">{film.title}</h1>
+            <h1 className="mb-1 mb-sm-3 pt-1 px-2">{film.title}</h1>
             <div className="px-2">
               <Row className="py-1">
                 <Col>
@@ -66,47 +82,69 @@ const SingleFilm = () => {
                 </Col>
               </Row>
               <Row className="py-1">
-                <Col>
-                  <h3>Producer:</h3>
-                  <span>{film.producer}</span>
+                <Col className="pt-1">
+                  <div className="flex-column cutoff-text">
+                    <h3 className="my-2">Appearances</h3>
+                    <ListOfShips shipsUrls={film.starships} />
+                  </div>
+                  {dynamicSize.mainWidth < 517 &&
+                    film.starships?.length >= 3 && (
+                      <input type="checkbox" className="expand-btn" />
+                    )}
+                  {dynamicSize.mainWidth > 517 &&
+                    film.starships?.length > 6 && (
+                      <input type="checkbox" className="expand-btn" />
+                    )}
                 </Col>
                 <Col>
-                  <h3>Release date</h3>
+                  <h3 className="my-2">Release date</h3>
                   <span>{film.release_date}</span>
                 </Col>
               </Row>
-              <Row className="py-1 flex-column">
-                <Col className="pt-1 cutoff-text">
-                  <h3 className="m-0 py-1">Ships</h3>
+              {/* <Row className="flex-column"> */}
+              {/* <div className="cutoff-text">
+                  <h3 className="my-2">Ships</h3>
                   {film.starships?.length > 0 ? (
                     <ListOfShips shipsUrls={film.starships} />
                   ) : (
                     <span>No ships for this character</span>
                   )}
-                </Col>
-                <input
-                  style={expandBtnStyles}
-                  type="checkbox"
-                  className="expand-btn"
-                />
-              </Row>
-              {/* <Row className="py-1 flex-column">
-                <Col className="pt-1 cutoff-text">
-                  <h3 className="m-0 py-1">Ships</h3>
-                  {film.starships?.length > 0 ? (
+                </div>
+                {dynamicSize.mainWidth < 517 && film.starships?.length >= 3 && (
+                  <input
+                    style={expandBtnStyles}
+                    type="checkbox"
+                    className="expand-btn"
+                  />
+                )}
+                {dynamicSize.mainWidth > 517 && film.starships?.length > 6 && (
+                  <input
+                    style={expandBtnStyles}
+                    type="checkbox"
+                    className="expand-btn"
+                  />
+                )} */}
+              {/* <Col className="pt-1">
+                  <div className="flex-column cutoff-text">
+                    <h3 className="my-2">Appearances</h3>
                     <ListOfShips shipsUrls={film.starships} />
-                  ) : (
-                    <span>There aren't ships for this character</span>
-                  )}
-                </Col>
-                <input type="checkbox" className="expand-btn" />
-              </Row> */}
+                  </div>
+                  {dynamicSize.mainWidth < 517 &&
+                    film.starships?.length >= 3 && (
+                      <input type="checkbox" className="expand-btn" />
+                    )}
+                  {dynamicSize.mainWidth > 517 &&
+                    film.starships?.length > 6 && (
+                      <input type="checkbox" className="expand-btn" />
+                    )}
+                </Col> */}
+              {/* </Row> */}
             </div>
           </div>
         </main>
       )}
     </>
-  )
-}
+  );
+};
 
-export default SingleFilm
+export default SingleFilm;
