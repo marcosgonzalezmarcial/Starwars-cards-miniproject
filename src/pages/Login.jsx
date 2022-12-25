7d602df3a084ee0fd0274295c31155390cfad3c0
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-12-25 06:54:33 +0000

    styles update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./form.scss";
 import yellowSwLogo from "../assets/yellow-sw-logo.svg";
 
 const initialUserDataState = { email: "", password: "" };
--- src/pages/NestedRoutes.jsx
+++ src/pages/NestedRoutes.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const NestedRoutes = () => {
-  return <Outlet />
-}
+  return <Outlet />;
+};
 
-export default NestedRoutes
+export default NestedRoutes;
