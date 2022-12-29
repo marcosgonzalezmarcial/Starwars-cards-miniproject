503e8b6ebf6f227c404a426668fa1f6252bad5d6
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-12-29 14:45:27 +0000

    Fix SectionNav refactor

--- src/components/Header/SectionNav/index.jsx
+++ src/components/Header/SectionNav/index.jsx
@@ -5,29 +5,28 @@ import "./SectionNav.scss";
 
 const SectionNav = () => {
 
-  const renderSections = useCallback(() =>
-    ["planets", "starships", "characters"].map(
-      (section) => (
-        <Col className="border-silver">
-          <Nav.Item>
-            <NavLink className="nav-link text-center py-1" to={section}>
-              {({ isActive }) => (
-                <>
-                  <div className="py-1 position-realitive">
-                    <div
-                      className={isActive ? "active-link-light-saber" : ""}
-                    ></div>
-                    <span>{section.toUpperCase()}</span>
-                  </div>
-                </>
-              )}
-            </NavLink>
-          </Nav.Item>
-        </Col>
-      ),
-      []
+
+
+  const renderSections = useCallback(() => ["planets", "starships", "characters"].map(
+    (section) => (
+      <Col className="border-silver">
+        <Nav.Item>
+          <NavLink className="nav-link text-center py-1" to={section}>
+            {({ isActive }) => (
+              <>
+                <div className="py-1 position-realitive">
+                  <div
+                    className={isActive ? "active-link-light-saber" : ""}
+                  ></div>
+                  <span>{section.toUpperCase()}</span>
+                </div>
+              </>
+            )}
+          </NavLink>
+        </Nav.Item>
+      </Col>
     )
-  );
+  ), [])
 
   return (
     <div className="border-wrapper">
