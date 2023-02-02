96e12277b37d980b5af464aa2ce4090cc694bb42
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2023-02-02 14:07:08 +0000

    update styles

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,7 @@
-import { Container } from "react-bootstrap";
-
 export default function ErrorPage() {
   return (
-    <Container className="mt-3">
-      <h1 className="display-3 text-center text-white">
-        Error 404: Página no encontrada
-      </h1>
-    </Container>
+    <h1 className="display-3 text-center text-white">
+      Error 404: Página no encontrada
+    </h1>
   );
 }
