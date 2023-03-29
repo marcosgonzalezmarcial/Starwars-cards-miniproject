dd422a1c09264b3bd64c57199beb142378d883e1
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2023-03-29 10:13:30 +0000

    Fix: SearchResults was only rendering one item even if the results were many

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,81 +1,49 @@
-import { useSearch } from 'hooks/useSearch'
-import { Link, useSearchParams } from 'react-router-dom'
+import { useSearch } from "hooks/useSearch";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function SearchResults() {
-  const [searchParams] = useSearchParams()
-  const searchCategory = searchParams.get('category')
-  const searchTerm = searchParams.get('searchTerm')
-  const { searchResultsItems } = useSearch({ searchCategory, searchTerm })
+  const [searchParams] = useSearchParams();
+  const searchCategory = searchParams.get("category");
+  const searchTerm = searchParams.get("searchTerm");
+  const { searchResultsItems } = useSearch({ searchCategory, searchTerm });
+  console.log({ searchResultsItems });
 
-  return searchResultsItems ? (
-    searchResultsItems.map((item) => (
-      <div className="grid-items">
-        <Link
-          className="grid-item"
-          key={item.model ?? item.name}
-          to={`/${searchCategory}/${item.name.replaceAll(' ', '~')}`}
+  return (
+    <>
+      {searchResultsItems && (
+        <div className="grid-items">
+          {searchResultsItems.map((item) => (
+            <Link
+              className="grid-item"
+              key={item.model ?? item.name}
+              to={`/${searchCategory}/${item.name.replaceAll(" ", "~")}`}
+            >
+              <div className="grid-item__hero">
+                <img
+                  className="grid-item__img"
+                  src={item.img_small || item.image || item.imgUrl}
+                  alt={item.name}
+                />
+              </div>
+              <div className="grid-item__info">
+                <h4>{item.name}</h4>
+                <p>{item.model ?? item.species}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
+      {!searchResultsItems && (
+        <div
+          style={{
+            marginTop: "5vh",
+            textAlign: "center",
+            color: "var(--primary-white)",
+          }}
         >
-          <div className="grid-item__hero">
-            <img
-              className="grid-item__img"
-              src={item.imgUrl || item.image}
-              alt={item.name}
-            />
-          </div>
-          <div className="grid-item__info">
-            <h4>{item.name}</h4>
-            <p>{item.model ?? item.species}</p>
-          </div>
-        </Link>
-      </div>
-    ))
-  ) : (
-    <div
-      style={{
-        marginTop: '5vh',
-        textAlign: 'center',
-        color: 'var(--primary-white)'
-      }}
-    >
-      <h1>No results found</h1>
-    </div>
-  )
-
-  // return (
-  //   <div className="grid-items">
-  //     {searchResultsItems ? (
-  //       // <div className="grid-items">
-  //       searchResultsItems.map((item) => (
-  //         <Link
-  //           className="grid-item"
-  //           key={item.model ?? item.name}
-  //           to={`/${searchCategory}/${item.name.replaceAll(' ', '~')}`}
-  //         >
-  //           <div className="grid-item__hero">
-  //             <img
-  //               className="grid-item__img"
-  //               src={item.imgUrl || item.image}
-  //               alt={item.name}
-  //             />
-  //           </div>
-  //           <div className="grid-item__info">
-  //             <h4>{item.name}</h4>
-  //             <p>{item.model ?? item.species}</p>
-  //           </div>
-  //         </Link>
-  //       ))
-  //     ) : (
-  //       // </div>
-  //       <div
-  //         style={{
-  //           marginTop: '5vh',
-  //           textAlign: 'center',
-  //           color: 'var(--primary-white)'
-  //         }}
-  //       >
-  //         <h1>No results found</h1>
-  //       </div>
-  //     )}
-  //   </div>
-  // )
+          <h1>No results found</h1>
+        </div>
+      )}
+    </>
+  );
 }
