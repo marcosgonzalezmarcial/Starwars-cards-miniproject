c55d518dd84402815c5d2a3781cc1ffd96510b8d
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2023-04-19 16:08:05 +0000

    Testing BackToTopBtn work in progress

--- src/components/BackToTopBtn/index.jsx
+++ src/components/BackToTopBtn/index.jsx
@@ -1,43 +1,41 @@
-import { useEffect, useRef } from "react";
-import "./BackToTopBtn.scss";
-import upChevronBtn from "assets/icons/up-chevron-btn.svg";
+import { useEffect, useRef } from 'react'
+import './BackToTopBtn.scss'
+import upChevronBtn from 'assets/icons/up-chevron-btn.svg'
 
 export function BackToTopBtn() {
-  const backToTopBtnRef = useRef(null);
+	const backToTopBtnRef = useRef(null)
 
-  useEffect(() => {
-    const backToTop = () => {
-      if (window.scrollY > 300) {
-        console.log("backToTop");
-        backToTopBtnRef.current.classList.add("show");
-      } else {
-        backToTopBtnRef.current.classList.remove("show");
-      }
-    };
-    if (backToTopBtnRef.current) {
-      window.addEventListener("scroll", backToTop);
-    }
-    return () => window.removeEventListener("scroll", backToTop);
-  }, []);
+	useEffect(() => {
+		const backToTop = () => {
+			if (window.scrollY > 300) {
+				backToTopBtnRef.current.classList.add('show')
+			} else {
+				backToTopBtnRef.current.classList.remove('show')
+			}
+		}
+		if (backToTopBtnRef.current) {
+			window.addEventListener('scroll', backToTop)
+		}
+		return () => window.removeEventListener('scroll', backToTop)
+	}, [])
 
-  return (
-    <button
-      onClick={() =>
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-        })
-      }
-      ref={backToTopBtnRef}
-      className="back-to-top-btn"
-      title="Back to top button"
-      role="button"
-    >
-      <img
-        className="back-to-top-btn__img"
-        src={upChevronBtn}
-        alt="search icon"
-      />
-    </button>
-  );
+	return (
+		<button
+			onClick={() =>
+				window.scrollTo({
+					top: 0,
+					behavior: 'smooth',
+				})
+			}
+			ref={backToTopBtnRef}
+			className="back-to-top-btn"
+			title="Back to top button"
+		>
+			<img
+				className="back-to-top-btn__img"
+				src={upChevronBtn}
+				alt="search icon"
+			/>
+		</button>
+	)
 }
--- src/components/BackToTopBtn/index.test.js
+++ src/components/BackToTopBtn/index.test.js
@@ -1,36 +1,65 @@
-import React from "react";
-import { render, fireEvent, screen, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import { BackToTopBtn } from "./index";
+import React from 'react'
+import { render, fireEvent, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { prettyDOM } from '@testing-library/dom'
 
-describe("BackToTopBtn component", () => {
-  afterEach(cleanup);
+import { BackToTopBtn } from './index'
 
-  it("renders without errors", () => {
-    render(<BackToTopBtn />);
-  });
+describe('BackToTopBtn component', () => {
+	let backToTopBtn
+	beforeEach(() => {
+		const { getByRole } = render(<BackToTopBtn />)
+		backToTopBtn = getByRole('button')
+	})
+	afterEach(cleanup)
 
-  it("has the correct title and role attributes", () => {
-    const { getByRole } = render(<BackToTopBtn />);
-    const backToTopBtn = getByRole("button");
-    expect(backToTopBtn).toHaveAttribute("title", "Back to top button");
-    expect(backToTopBtn).toHaveAttribute("role", "button");
-  });
+	// Tests that the BackToTopBtn component is rendered with the correct class and image.
+	it('test_renders_back_to_top_btn_with_correct_class_and_image', () => {
+		expect(backToTopBtn).toHaveClass('back-to-top-btn')
+		expect(backToTopBtn.firstChild).toContainHTML(
+			'<img class="back-to-top-btn__img" src="up-chevron-btn.svg" alt="search icon">'
+		)
+	})
 
-  it("is hidden when the page first loads", () => {
-    const { queryByRole } = render(<BackToTopBtn />);
-    const backToTopBtn = queryByRole("button");
-    expect(backToTopBtn).not.toHaveClass("show");
-  });
+	// Tests that the BackToTopBtn component is visible when the user scrolls past 300px.
+	it('test_back_to_top_btn_is_visible_when_scrolled_past_300px', () => {
+		expect(backToTopBtn).not.toHaveClass('show')
+		window.scrollY = 400
+		fireEvent.scroll(window)
+		expect(backToTopBtn).toHaveClass('show')
+		expect(backToTopBtn).toBeInTheDocument()
+	})
 
-  it("scrolls the page to the top when clicked", () => {
-    window.scrollTo = jest.fn();
-    const { getByRole } = render(<BackToTopBtn />);
-    const backToTopBtn = getByRole("button");
-    fireEvent.click(backToTopBtn);
-    expect(window.scrollTo).toHaveBeenCalledWith({
-      top: 0,
-      behavior: "smooth",
-    });
-  });
-});
+	// Tests that clicking the BackToTopBtn scrolls the window to the top smoothly and the button is hidden.
+	it('test_back_to_top_btn_scrolls_to_top', () => {
+		window.scrollTo = jest.fn()
+		fireEvent.click(backToTopBtn)
+		expect(window.scrollTo).toHaveBeenCalledWith({ top: 0, behavior: 'smooth' })
+		expect(backToTopBtn).not.toHaveClass('show')
+	})
+
+	// it('has the correct title and role attributes', () => {
+	// 	const { getByRole } = render(<BackToTopBtn />)
+	// 	const backToTopBtn = getByRole('button')
+	// 	expect(backToTopBtn).toHaveAttribute('title', 'Back to top button')
+	// 	expect(backToTopBtn).toHaveAttribute('role', 'button')
+	// })
+
+	// it('is hidden when the page first loads', () => {
+	// 	const { queryByRole } = render(<BackToTopBtn />)
+	// 	const backToTopBtn = queryByRole('button')
+	// 	console.log(prettyDOM(backToTopBtn.parentNode))
+	// 	expect(backToTopBtn).not.toHaveClass('show')
+	// })
+
+	// it('scrolls the page to the top when clicked', () => {
+	// 	window.scrollTo = jest.fn()
+	// 	const { getByRole } = render(<BackToTopBtn />)
+	// 	const backToTopBtn = getByRole('button')
+	// 	fireEvent.click(backToTopBtn)
+	// 	expect(window.scrollTo).toHaveBeenCalledWith({
+	// 		top: 0,
+	// 		behavior: 'smooth',
+	// 	})
+	// })
+})
